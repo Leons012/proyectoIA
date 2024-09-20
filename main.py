1f83c7c35cde7f5478bea0927b6cb4d700eb7e74
David Camacho <davidalejo430@gmail.com>
2024-09-20 11:58:33 -0500

    version por clases

---
+++ main.py
@@ -0,0 +1,259 @@
+from tkinter import *
+import ttkbootstrap as tb
+from PIL import ImageTk, Image
+
+class App(tb.Window):
+    def __init__(self, theme, color):
+        super().__init__(themename=theme)
+
+        WIDTHSCREEN = self.winfo_screenwidth()
+        HEIGHTSCREEN = self.winfo_screenheight()
+
+        self.defineColors(theme)
+        self.attributes('-fullscreen', True)
+        self.attributes('-transparentcolor', self.bg)
+
+        self.presentation = FramePresentation(self, color, 600, 400, WIDTHSCREEN, HEIGHTSCREEN)
+
+        self.after(2000,self.replace)
+
+    def replace(self):
+        self.presentation.destroy()
+        self.menu = Menu(self)
+        self.menu.place(relx=0.6, y=0, relwidth=0.4, relheight=1)
+
+        self.grafico = GraphicFrame(self)
+        self.grafico.place(x=0, y=0, relwidth=0.6, relheight=1)
+        self.attributes('-transparentcolor', "")
+    
+
+    def defineColors(self, theme):
+        lightmode = ("cosmo", "flatly", "journal", "litera", "minty", "lumen", "sandstone", "yeti", "pulse", "united", "cerculean")
+        if (theme in lightmode):
+            self.bg= "#ffffff"
+        elif (theme == "simplex"):
+            self.bg = "#fcfcfc"
+        elif (theme == "morph"):
+            self.bg = "#D9E3F1"
+        elif (theme == "light"):
+            self.bg= "#7e8081"
+        elif (theme == "solar"):
+            self.bg = "#002B36"
+        elif (theme == "darkly"):
+            self.bg = "#222222"
+        elif (theme == "superhero"):
+            self.bg = "#2b3e50"
+        elif (theme == "cyborg"):
+            self.bg = "#060606"
+        elif (theme== "vapor"):
+            self.bg = "#190831"
+        
+class FramePresentation(tb.Frame):
+    def __init__(self, master, color, width, height, widthscreen, heightscreen):
+        super().__init__(master=master, bootstyle=color, width=width, height=height)
+        xposition = (widthscreen-width)/2
+        yposition = (heightscreen-height)/2
+        self.place(x=xposition, y=yposition)
+
+        tb.Label(self, text="Presentado por:", bootstyle=f"{color}inverse", font=("Segoe UI Light", 20)).place(relx=0.3, y=30)
+        tb.Label(self, text="David Alejandro Camacho León", bootstyle=f"{color}inverse", font=("Segoe UI Light", 15)).place(x=10, rely=0.25)
+        tb.Label(self, text="Kevin Guerrero Penagos", bootstyle=f"{color}inverse", font=("Segoe UI Light", 15)).place(x=10, rely=0.45)
+        tb.Label(self, text="Andres Felipe Vasquez", bootstyle=f"{color}inverse", font=("Segoe UI Light", 15)).place(x=10, rely=0.65)
+
+class GraphicFrame(tb.Frame):
+    def __init__(self, master):
+        super().__init__(master=master, bootstyle="light")
+
+        tb.Frame(self, bootstyle="secondary").pack(padx=43, pady=43,fill="both", expand=True)
+
+        self.animated_frame = SlidePanel(self, 0.96, 0.5)
+
+        button_style = tb.Style()
+        button_style.configure("primary.TButton", font=("Segoe UI Light", 12))
+
+        self.resultados = tb.Button(self.animated_frame, bootstyle="primary", style="primary_TButton", text="Resultados", width=113, state="disabled", command=self.animated_frame.animate)
+        self.resultados.pack()
+
+class SlidePanel(Frame):
+    def __init__(self, parent, start_pos, end_pos):
+        super().__init__(master=parent)
+
+        self.start_pos = start_pos
+        self.end_pos = end_pos
+        self.height = 1-abs(start_pos-end_pos)
+
+        self.pos = start_pos
+        self.in_start_pos = True
+
+        self.place(relx=0, rely=self.start_pos, relheight=self.height, relwidth=1)
+    
+    def animate(self):
+        if self.in_start_pos:
+            self.animate_forward()
+        else:
+            self.animate_backwards()
+        
+    def animate_forward(self):
+        if self.pos > self.end_pos:
+            self.pos -= 0.05
+            self.place(relx=0, rely=self.pos, relheight=self.height, relwidth=1)
+            self.after(10, self.animate_forward)
+        else:
+            self.in_start_pos = False
+
+    def animate_backwards(self):
+        if self.pos < self.start_pos:
+            self.pos += 0.05
+            self.place(relx=0, rely=self.pos, relheight=self.height, width=1)
+            self.after(10, self.animate_backwards)
+        else:
+            self.in_start_pos = True
+
+class Menu(tb.Frame):
+    def __init__(self, master):
+        super().__init__(master=master, bootstyle="dark")
+        self
+        self.blank = True
+
+        IconButton(self, bootstyle="danger", path="./images/close.png", width=50, height=50, command=close).pack(padx=20, pady=20, anchor=NE)
+
+        SectionTitle(self, title="Seleccion de Ruta")
+
+        self.route = routeSelector(self)
+
+        SectionTitle(self, "Método de Búsqueda")
+
+        self.search_meth = SearchMethod(self)
+
+        frame = tb.Frame(self, bootstyle="dark")
+        frame.pack()
+
+        IconButton(frame, bootstyle="success", path="./images/search.png", width=75, height=75, command=search).grid(row=0, column=0, padx=100)
+        IconButton(frame, bootstyle="warning", path="./images/clear.png", width=75, height=75, command=clear).grid(row=0, column=1, padx=100)
+
+class IconButton(tb.Button):
+    def __init__(self, master, bootstyle, width, height, path, command=None):
+        super().__init__(master=master, bootstyle=bootstyle, command=command)
+        self.width = width
+        self.height = height
+        self.path = path
+
+        self.icon = self.resizeImage()
+
+        self.config(image=self.icon)
+        
+        
+    def resizeImage(self):
+        image = Image.open(self.path).resize((self.width, self.height))
+        image_tk = ImageTk.PhotoImage(image)
+        return image_tk
+
+class SectionTitle(tb.Frame):
+    def __init__(self, master, title):
+        super().__init__(master=master, bootstyle="dark")
+
+        tb.Label(self, text=title, font=("Segoe UI Light", 20), bootstyle="darkinverse").pack(pady=20)
+
+        tb.Separator(self, bootstyle="light", orient="horizontal").pack(fill=X, padx=75, pady= 20)
+
+        self.pack(fill=X, padx=75, pady=20)
+
+class routeSelector(tb.Frame):
+    def __init__(self, master):
+        super().__init__(master=master, bootstyle="dark")
+
+        self.in_blank = True
+
+        self.barrios = []
+        for i in range(1, 11):
+            self.barrios.append(f"Barrio {i}")
+
+        self.barrio1 = tb.Combobox(self, bootstyle="light", values=self.barrios, state="readonly", font=(("Segoe UI Bold", 12)))
+        self.barrio1.grid(row=0, column=0, padx=20)
+        self.barrio1.bind("<<ComboboxSelected>>", self.deleteOption)
+
+        tb.Label(self, text="a", bootstyle="darkinverse", font=("Segoe UI Light", 20)).grid(row=0, column=1, padx=20)
+
+        self.barrio2 = tb.Combobox(self, bootstyle="light", values=self.barrios, state="readonly", font=("Segoe UI Bold", 12))
+        self.barrio2.grid(row=0, column=2, padx=20)
+        self.barrio2.bind("<<ComboboxSelected>>", self.deleteOption)
+
+        self.pack()
+    
+    def deleteOption(self, e):
+        # Se crea nuevamente la lista cada vez que se selecciona una opcion
+        self.barrios = []
+        for i in range(1, 11):
+            self.barrios.append(f"Barrio {i}")
+        
+        # Si el barrio 1 está seleccionado y se encuentra en la lista, se elimina
+        if ((self.barrio1.get() != "")):
+            self.barrios.remove(self.barrio1.get())
+        # Si el barrio 2 está seleccionado se elimina
+        if ((self.barrio2.get() != "")):
+            self.barrios.remove(self.barrio2.get())
+
+        # Se actualiza los combobox con las opciones que no fueron eliminadas
+        self.barrio1.config(values=self.barrios)
+        self.barrio2.config(values=self.barrios)
+
+    def inBlank(self):
+        if ((self.barrio1.get() != "") and (self.barrio2.get() != "")):
+            self.in_blank = False
+        
+        return self.in_blank
+    
+    def setBlank(self):
+        self.in_blank = True
+        self.barrios = []
+        for i in range(1, 11):
+            self.barrios.append(f"Barrio {i}")
+        self.barrio1.config(values=self.barrios)
+        self.barrio2.config(values=self.barrios)
+        self.barrio1.set("")
+        self.barrio2.set("")
+
+class SearchMethod(tb.Frame):
+    def __init__(self, master):
+        super().__init__(master=master, bootstyle="dark")
+
+        self.in_blank = True
+
+        radiobutton_style = tb.Style()
+        radiobutton_style.configure("primary-outline-toolbutton.TRadioButton", font=("Segoe UI Bold"))
+
+        self.searches = ("amplitud", "profundidad", "coste uniforme", "profundidad iterativa")
+        
+        self.search = StringVar()
+        for i in range(4):
+            tb.Radiobutton(self, style="primary-outline-toolbutton_TRadioButton", variable=self.search, text=self.searches[i], value=self.searches[i], width=50).pack(pady=20)
+        
+        self.pack(pady=40)
+
+    def inBlank(self):
+        if (self.search.get() != ""):
+            self.in_blank = False
+
+        return self.in_blank
+
+    def setBlank(self):
+        self.search.set("")
+        self.in_blank = True
+
+def search():
+    if (not(main.menu.route.inBlank()) and not(main.menu.search_meth.inBlank())):
+        main.grafico.resultados.config(state="enabled")
+
+def clear():
+    if (not(main.menu.route.inBlank()) or not(main.menu.search_meth.inBlank())):
+        main.menu.route.setBlank()
+        main.menu.search_meth.setBlank()
+    main.grafico.animated_frame.animate_backwards()
+    main.grafico.resultados.config(state="disabled")
+
+def close():
+    main.destroy()
+
+main = App("lumen", "dark")
+    
+main.mainloop()
